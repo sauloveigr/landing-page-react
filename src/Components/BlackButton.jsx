de0636fba3762b7ef6ec8aaa9ee523545764aacf
Saulo <sauloveigr@hotmail.com>
2022-01-18 15:39:14 -0300

    Create button hover

--- src/Components/BlackButton.jsx
+++ src/Components/BlackButton.jsx
@@ -3,11 +3,17 @@ import styled from "styled-components";
 const BlackButton = styled.button`
     width: 11.25rem;
     height: 4.063rem;
-    border: 1px solid white;
-    color: white;
+    border: 1px solid #fff;
+    color: rgba(255, 255, 255, 0.7);
     background-color: #181818;
     font-size: 1.07rem;
     cursor: pointer;
+    transition: 0.5s;
+
+    &:hover {
+        color: #fff;
+        border: 1px solid #4452fe;
+    }
 `;
 
 export default BlackButton;
--- src/Components/PurpleButton.jsx
+++ src/Components/PurpleButton.jsx
@@ -3,11 +3,17 @@ import styled from "styled-components";
 const PurpleButton = styled.button`
     width: 11.25rem;
     height: 4.063rem;
-    color: white;
-    background-color: #4452FE;
+    border: 1px solid #4452fe;
+    color: rgba(255, 255, 255, 0.7);
+    background-color: #4452fe;
     font-size: 1.07rem;
     cursor: pointer;
-    border: none;
+    transition: 0.5s;
+
+    &:hover {
+        color: #fff;
+        border: 1px solid #fff;
+    }
 `;
 
 export default PurpleButton;
