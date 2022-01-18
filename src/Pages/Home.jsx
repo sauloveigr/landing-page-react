60b9ad71ec93245f4688cee7bb254a8122001eb6
Saulo <sauloveigr@hotmail.com>
2022-01-18 19:23:12 -0300

    Test

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import backgroundImg from "../assets/screens.png";
-import backgroundTest from "../assets/bgTest.png";
+// import backgroundTest from "../assets/bgTest.png";
+import homeBackground from "../assets/homeBackground.png";
 import BlackButton from "../Components/BlackButton";
 import PurpleButton from "../Components/PurpleButton";
 
@@ -25,6 +26,7 @@ function Home() {
                         <BlackButton>Learn More</BlackButton>
                     </WrapperButton>
                 </WrapperDiv>
+                {/* <GradientDiv /> */}
             </HomeSection>
         </div>
     );
@@ -34,12 +36,22 @@ export default Home;
 
 export const HomeSection = styled.section`
     /* background-image: url(${backgroundImg}); */
-    background-image: url(${backgroundTest});
+    background-image: url(${homeBackground});
     background-repeat: no-repeat;
     background-position-y: bottom;
-    background-position-x: 5.625rem;
+    background-position-x: 10.969rem;
     height: 89vh;
 `;
+
+// export const GradientDiv = styled.div`
+//     width: 100%;
+//     height: 50%;
+//     position: absolute;
+//     /* top: 912px; */
+//     /* z-index: 1; */
+//     /* background-image: linear-gradient(to top, #1414149c 25%, 75%, rgba(0, 0, 0, 0)); */
+//     opacity: 0.1;
+// `;
 
 export const WrapperDiv = styled.div`
     width: 100%;
